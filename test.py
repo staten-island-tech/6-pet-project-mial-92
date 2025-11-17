007b9e0780fe3bdfb3c7112b478fe6491747316c
mial-92 <mialin0170@gmail.com>
2025-11-17 10:16:27 -0500

    Update test.py

--- test.py
+++ test.py
@@ -19,36 +19,69 @@ class BankAccount:
     def show_balance(self):
         print(f"{self.owner} has ${self.__balance}")
  """
-
-class pet:
-    def __init__(pet, animal, happiness, hunger, clean):
-        pet.__happiness = happiness
-        pet.__hunger = hunger
-        pet.__clean = clean
-    def play(pet, happiness):
-        happiness = 75
-        response = input(f"Do you want to play with {pet_name}?")
-        if response == "yes" in response:
-            pet.__happiness += 5
-            pet.__hunger -= 5
-            pet.__clean -= 10
-            print(f"{pet_name} is happy!")
-        elif response == "no":
-            pet.__happiness = pet.__happiness
-        else: 
-            print("Answer Invalid. Please Try Again")
-        if pet.__happiness > 100:
-            pet.__happiness = 100
-    def feed(pet, hunger):
-        hunger = 100
-        response2 = input(f"{pet_name}'s getting hungry... WOuld you like to feed them?")
-
-         
 name = input("What's your name?")
 print(f"Hi, {name}!")
 pet_name = input("what would you like to name your pet?")
 print(f"Very nice! Meet your class pet, goldfish, {pet_name}. Take good care of them!")
 input(f"Do you want to play with {pet_name}?")
+
+class pet:
+    def __init__(pet, happiness, hunger, clean, age):
+        pet.__happiness = happiness
+        pet.__hunger = hunger
+        pet.__clean = clean
+        pet.__age = age
+    def play(pet):
+        pet.__happiness = 75
+        response = input(f"Do you want to play with {pet_name}?")
+        if response == "yes" in response:
+            pet.__happiness += 5
+            pet.__hunger -= 5
+            pet.__clean -= 12
+            print(f"{pet_name} is happy!")
+        elif response == "no":
+            pet.__happiness += 0
+        else: 
+            print("Answer Invalid. Please Try Again")
+        if pet.__happiness > 100:
+            pet.__happiness = 100
+        if pet.__clean > 100:
+            pet.__clean = 100
+    def feed(pet):
+        pet.__hunger = 100
+        response2 = input(f"{pet_name}'s getting hungry... Would you like to feed them?")
+        if response2 == "yes" in response2:
+            pet.__happiness += 2
+            pet.__hunger += 5
+            pet.__clean -= 10
+        elif response2 == "no" in response2:
+            pet.__happiness += 0 
+            pet.__hunger -= 8
+            pet.__clean += 0
+        else: 
+            print("Answer Invalid. Please Try Again")
+        if pet.__happiness > 100:
+            pet.__happiness = 100
+        if pet.__clean > 100:
+            pet.__clean = 100
+    def clean(pet):
+        pet.__clean = 100
+        response3 = input(f"{pet_name}'s getting dirty... Would you like to clean them?")
+        if response3 == "yes" in response3:
+            pet.__happiness += 10
+            pet.__hunger += 0
+            pet.__clean += 20
+        elif response3 == "no" in response3:
+            pet.__happiness -= 5
+            pet.__hunger += 0
+            pet.__clean -= 10
+        else: 
+            print("Answer Invalid. Please Try Again")
+        if pet.__happiness > 100:
+            pet.__happiness = 100
+        if pet.__clean > 100:
+            pet.__clean = 100
+
 
 """ class pet:
     def __init__
