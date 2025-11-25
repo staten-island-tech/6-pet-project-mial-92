06cf03b0aeae4b474cb70bcd869b73291590f006
mial-92 <mialin0170@gmail.com>
2025-11-25 09:49:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -27,12 +27,12 @@ class pet:
         pet.__age = age
     def play(pet):
         pet.__happiness = 75
-        response = input(f"Do you want to play with {pet_name}?")
+        response = input(f"Do you want to play with {name2}?")
         if response == "yes" in response:
             pet.__happiness += 5
             pet.__hunger -= 5
             pet.__clean -= 12
-            print(f"{pet_name} is happy!")
+            print(f"{name2} is happy!")
         elif response == "no":
             pet.__happiness += 0
         else: 
@@ -43,7 +43,7 @@ class pet:
             pet.__clean = 100
     def feed(pet):
         pet.__hunger = 100
-        response2 = input(f"{pet_name}'s getting hungry... Would you like to feed them?")
+        response2 = input(f"{name2}'s getting hungry... Would you like to feed them?")
         if response2 == "yes" in response2:
             pet.__happiness += 2
             pet.__hunger += 5
@@ -60,7 +60,7 @@ class pet:
             pet.__clean = 100
     def clean(pet):
         pet.__clean = 100
-        response3 = input(f"{pet_name}'s getting dirty... Would you like to clean them?")
+        response3 = input(f"{name2}'s getting dirty... Would you like to clean them?")
         if response3 == "yes" in response3:
             pet.__happiness += 10
             pet.__hunger += 0
@@ -71,32 +71,34 @@ class pet:
             pet.__clean -= 10
         else: 
             print("Answer Invalid. Please Try Again")
-        def show_stats(pet):
-            pet.__age == 24
+    def status(pet):
+        pet.__age == 24
+        answer4 = input("It's the end of the day! Your goldfish is going to sleep. Would you like to see their stats?")
+        if answer4 == "yes":
+            print(f"{pet.__name}'s happiness is at {pet.__happiness}%. It's hunger is at {pet.__hunger}%. It's clean-ness is at {pet.__clean}%. Your pet is {pet.__age} years old.")
             pet.__age += 1
-            answer4 = input("It's the end of the day! Your goldfish is going to sleep. Would you like to see their stats?")
-            if answer4 == "yes":
-                print(f"{pet.__name}'s happiness is at {pet.__happiness}%. It's hunger is at {pet.__hunger}%. It's clean-ness is at {pet.__clean}%. Your pet is {pet.__age} years old.")
+        else:
+            pet.__age += 1
 
 name = input("What's your name?")
 print(f"Hi, {name}!")
-pet_name = input("what would you like to name your pet?")
-print(f"Very nice! Meet your class pet, goldfish, {pet_name}. Take good care of them!")
-input(f"Do you want to play with {pet_name}?")
-goldfish = pet({pet_name}, 85, 85, 85, 24)
-while goldfish.age > 350:
+name2 = input("what would you like to name your pet?")
+goldfish = pet({name2},75,100,100,24)
+print(f"Very nice! Meet your class pet, goldfish, {name2}. Take good care of them!")
+input(f"Do you want to play with {name2}?")
+while goldfish.age < 350:
     goldfish.play()
     goldfish.feed()
     goldfish.clean()
-    
-if pet.__happiness > 100:
-            pet.__happiness = 100
-if pet.__clean > 100:
-            pet.__clean = 100
-if pet.__hunger > 125:
-            print(f"{pet_name} died due to obesity :C")
-if pet.__hunger > 10:
-            print(f"{pet_name} died due to starvation :C")
+    goldfish.status()
+    if pet.__happiness > 100:
+        pet.__happiness = 100
+    if pet.__clean > 100:
+        pet.__clean = 100
+    if pet.__hunger > 125:
+        print(f"{name2} died due to obesity :C")
+    elif pet.__hunger > 10:
+        print(f"{name2} died due to starvation :C")
 
 
 
