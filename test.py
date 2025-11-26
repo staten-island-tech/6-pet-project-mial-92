ce651a0a984f097ebd22cec74480d60f8a736846
mial-92 <mialin0170@gmail.com>
2025-11-26 10:17:10 -0500

    Update test.py

--- test.py
+++ test.py
@@ -20,104 +20,79 @@ class BankAccount:
         print(f"{self.owner} has ${self.__balance}")"""
 
 class pet:
-    def __init__(pet, happiness, hunger, clean, age):
-        pet.__happiness = happiness
-        pet.__hunger = hunger
-        pet.__clean = clean
-        pet.__age = age
-    def play(pet):
-        pet.__happiness = 75
+    def __init__(self, happiness, hunger, clean, age):
+        self.__happiness = happiness
+        self.__hunger = hunger
+        self.__clean = clean
+        self.age = age
+    def play(self):
         response = input(f"Do you want to play with {name2}?")
         if response == "yes" in response:
-            pet.__happiness += 5
-            pet.__hunger -= 5
-            pet.__clean -= 12
+            self.__happiness += 5
+            self.__hunger -= 5
+            self.__clean -= 12
             print(f"{name2} is happy!")
         elif response == "no":
-            pet.__happiness += 0
+            self.__happiness += 0
         else: 
             print("Answer Invalid. Please Try Again")
-        if pet.__happiness > 100:
-            pet.__happiness = 100
-        if pet.__clean > 100:
-            pet.__clean = 100
-    def feed(pet):
-        pet.__hunger = 90
+
+    def feed(self):
         response2 = input(f"{name2}'s getting hungry... Would you like to feed them?")
         if response2 == "yes" in response2:
-            pet.__happiness += 2
-            pet.__hunger += 5
-            pet.__clean -= 10
+            self.__happiness += 2
+            self.__hunger += 5
+            self.__clean -= 10
         elif response2 == "no" in response2:
-            pet.__happiness += 0 
-            pet.__hunger -= 8
-            pet.__clean += 0
+            self.__happiness += 0 
+            self.__hunger -= 8
+            self.__clean += 0
         else: 
             print("Answer Invalid. Please Try Again")
-    def clean(pet):
-        pet.__clean = 95
+
+    def clean(self):
         response3 = input(f"{name2}'s getting dirty... Would you like to clean them?")
         if response3 == "yes" in response3:
-            pet.__happiness += 10
-            pet.__hunger += 0
-            pet.__clean += 20
+            self.__happiness += 10
+            self.__hunger += 0
+            self.__clean += 20
         elif response3 == "no" in response3:
-            pet.__happiness -= 5
-            pet.__hunger += 0
-            pet.__clean -= 10
+            self.__happiness -= 5
+            self.__hunger += 0
+            self.__clean -= 10
         else: 
             print("Answer Invalid. Please Try Again")
-    def status(pet):
-        pet.__age = 24
+
+    def status(self):
         answer4 = input("It's the end of the day! Your goldfish is going to sleep. Would you like to see their stats?")
         if answer4 == "yes":
-            print(f"{name2}'s happiness is at {pet.__happiness}%. It's hunger is at {pet.__hunger}%. It's clean-ness is at {pet.__clean}%. Your pet is {pet.__age} years old.")
-            pet.__age += 1
+            print(f"{name2}'s happiness is at {self.__happiness}%. It's hunger is at {self.__hunger}%. It's clean-ness is at {self.__clean}%. Your pet is {self.age} years old.")
+            self.age += 1
         else:
-            pet.__age += 1
+            self.age += 1
 
 name = input("What's your name?")
 print(f"Hi, {name}!")
 name2 = input("what would you like to name your pet?")
 goldfish = pet(70, 90, 95, 24)
 print(f"Very nice! Meet your class pet, goldfish, {name2}. Take good care of them!")
-input(f"Do you want to play with {name2}?")
-while pet.__age <= 70:
+while goldfish.age <= 70:
     goldfish.play()
     goldfish.feed()
     goldfish.clean()
     goldfish.status()
-    if pet.__happiness > 100:
-        pet.__happiness = 100
-    if pet.__clean > 100:
-        pet.__clean = 100
-    if pet.__hunger > 125:
+    if goldfish.__hunger > 125:
         print(f"{name2} died due to obesity :C")
         break
-    elif pet.__hunger > 10:
+    elif goldfish.__hunger > 10:
         print(f"{name2} died due to starvation :C")
         break
-    elif pet.__age > 70:
+    elif goldfish.age > 70:
         print(f"{name2} died due to old age.")
         break
 
 
 
-
-"""class pet:
-    def __init__
-        animal.happiness = happiness
-        animal.hunger = hunger
-        animal.clean = clean
-    def deposit(animal, happiness):
-        animal._balance1 += happiness
-    def deposit(animal, hunger):
-        animal._balance2 += hunger
-    def deposit(animal, clean):
-        animal._balance3 += clean
-
-    def show_balance(pet_name):
-        print(f"{pet_name} is at {animal._balance1}% happiness")  """
 """ 
 def verifyemail():
     email = input("What is your email?")
