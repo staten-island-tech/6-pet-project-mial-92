0be7cd88c600a99519efb8c1f8a3a9b1b2aadba3
mial-92 <mialin0170@gmail.com>
2025-12-01 09:52:42 -0500

    Update test.py

--- test.py
+++ test.py
@@ -21,52 +21,52 @@ class BankAccount:
 
 class pet:
     def __init__(self, happiness, hunger, clean, age):
-        self.__happiness = happiness
-        self.__hunger = hunger
-        self.__clean = clean
+        self.happiness = happiness
+        self.hunger = hunger
+        self.clean = clean
         self.age = age
     def play(self):
         response = input(f"Do you want to play with {name2}?")
         if response == "yes" in response:
-            self.__happiness += 5
-            self.__hunger -= 5
-            self.__clean -= 12
+            self.happiness += 5
+            self.hunger -= 5
+            self.clean -= 12
             print(f"{name2} is happy!")
         elif response == "no":
-            self.__happiness += 0
+            self.happiness += 0
         else: 
             print("Answer Invalid. Please Try Again")
 
     def feed(self):
         response2 = input(f"{name2}'s getting hungry... Would you like to feed them?")
         if response2 == "yes" in response2:
-            self.__happiness += 2
-            self.__hunger += 5
-            self.__clean -= 10
+            self.happiness += 2
+            self.hunger += 5
+            self.clean -= 10
         elif response2 == "no" in response2:
-            self.__happiness += 0 
-            self.__hunger -= 8
-            self.__clean += 0
+            self.happiness += 0 
+            self.hunger -= 8
+            self.clean += 0
         else: 
             print("Answer Invalid. Please Try Again")
 
-    def clean(self):
+    def tidy(self):
         response3 = input(f"{name2}'s getting dirty... Would you like to clean them?")
         if response3 == "yes" in response3:
-            self.__happiness += 10
-            self.__hunger += 0
-            self.__clean += 20
+            self.happiness += 10
+            self.hunger += 0
+            self.clean += 20
         elif response3 == "no" in response3:
-            self.__happiness -= 5
-            self.__hunger += 0
-            self.__clean -= 10
+            self.happiness -= 5
+            self.hunger += 0
+            self.clean -= 10
         else: 
             print("Answer Invalid. Please Try Again")
 
     def status(self):
         answer4 = input("It's the end of the day! Your goldfish is going to sleep. Would you like to see their stats?")
         if answer4 == "yes":
-            print(f"{name2}'s happiness is at {self.__happiness}%. It's hunger is at {self.__hunger}%. It's clean-ness is at {self.__clean}%. Your pet is {self.age} years old.")
+            print(f"{name2}'s happiness is at {self.happiness}%. It's hunger is at {self.hunger}%. It's clean-ness is at {self.clean}%. Your pet is {self.age} years old.")
             self.age += 1
         else:
             self.age += 1
@@ -79,12 +79,12 @@ print(f"Very nice! Meet your class pet, goldfish, {name2}. Take good care of the
 while goldfish.age <= 70:
     goldfish.play()
     goldfish.feed()
-    goldfish.clean()
+    goldfish.tidy()
     goldfish.status()
-    if goldfish.__hunger > 125:
+    if goldfish.hunger > 125:
         print(f"{name2} died due to obesity :C")
         break
-    elif goldfish.__hunger > 10:
+    elif goldfish.hunger > 10:
         print(f"{name2} died due to starvation :C")
         break
     elif goldfish.age > 70:
